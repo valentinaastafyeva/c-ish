f75ccdb02b43759f5bc9e6cd8b59f065e223518a
valentina <astafjewa.valentina@gmail.com>
2022-09-15 09:31:52 +0300

    hello

---
+++ ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("What's your name?");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "lilly")
+{
+    Console.WriteLine("Ooooh myyyyyyy that's Lilly!");
+}
+else
+{
+    Console.Write("Hi ");
+    Console.WriteLine(username);
+}
